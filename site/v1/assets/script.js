f8417c8b7a009c1bac863806fb61a6869204ed50
mrgne1 <mrgne1@gmail.com>
2024-11-02 14:32:01 -0400

    Updated the frontend styles

--- site/v1/assets/script.js
+++ site/v1/assets/script.js
@@ -4,3 +4,9 @@
         const newUrl = url.split("/v1/")[0] + "/v1/";
         window.location.assign(newUrl);
     }
+
+    function copyElementValue(id) {
+        const link = document.getElementById(id).value;
+        navigator.clipboard.writeText(link);
+    }
+
